ce12038c905a2dd971879074efa96fceb7547420
fanghao <hifanghao@163.com>
2023-07-14 15:15:59 +0800

    修改消息队列路由

--- rabbitmq-consumer/src/main/java/com/hifanghao/rabbitdemo/config/RabbitMQConstantUtil.java
+++ rabbitmq-consumer/src/main/java/com/hifanghao/rabbitdemo/config/RabbitMQConstantUtil.java
@@ -15,12 +15,12 @@ public class RabbitMQConstantUtil {
     /**
      * 取消订单 队列名称 routingkey
      */
-    public static final String CANCEL_ORDER = "cancel-order4";
+    public static final String CANCEL_ORDER = "cancel-order";
 
     /**
      * 自动确认订单 队列名称\routingkey
      */
-    public static final String CONFIRM_ORDER = "confirm-order4";
+    public static final String CONFIRM_ORDER = "confirm-order";
 
 
 
